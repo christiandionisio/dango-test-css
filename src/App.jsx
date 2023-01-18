807cc873048ba3e42a2517a66cd78c8f4903cb8b
Christian Dionisio <christiandionisio9432@gmail.com>
2023-01-17 22:13:52 -0500

    dango test2 responsive

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { CardItem } from './components/CardItem';
+
+function App() {
+  return (
+    <div>
+      <div className="cards">
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+        <CardItem />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const CardItem = () => {
+  return (
+    <div class="card">
+        <div className="card-header">
+            <img src="https://cdn.shopify.com/s/files/1/0272/4714/9155/products/aactaril-soap_1200x1200.jpg?v=1622098133" width={400} height={300} />
+            <h2>Tourmaline & Eucalyptus Bar Soap</h2>
+        </div>
+        <div className="card-body">
+            <div className="price">
+                <h3>$12</h3>
+                <input type="text" value={'2'}/>
+            </div>
+            <p>Recharge your skin with the super energizing power of finely crushed tourmaline powder blended with natural complexion</p>
+        </div>
+
+        <div className="card-footer">
+            <button>Add to cart</button>
+            <button className='btn-white'>Learn More</button>
+        </div>
+    </div>
+  )
+}
